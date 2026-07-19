6d746e3779669e6432c835806c073659d37e123d
Fauzy <fauzyuji87@gmail.com>
2026-07-19 12:13:46 +0700

    Update login page meta description

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html lang="en" class="material-style layout-fixed">
+<html lang="id" class="material-style layout-fixed">
 
 <head>
     <title>Sistem Informasi KSP Pekali 99</title>
@@ -9,33 +9,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+
     <meta name="description"
-        content="Empire Bootstrap admin template made using Bootstrap 4, it has tons of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+        content="Sistem Informasi Koperasi Simpan Pinjam Pekali 99 untuk mengelola data nasabah, pinjaman, angsuran, dan laporan koperasi secara digital.">
     <meta name="keywords"
-        content="Empire, bootstrap admin template, bootstrap admin panel, bootstrap 4 admin template, admin template">
-    <meta name="author" content="Srthemesvilla" />
-    <link rel="icon" type="image/x-icon" href="assets/img/logo-koperasi.png">
+        content="KSP Pekali 99, koperasi simpan pinjam, sistem informasi koperasi, pinjaman, angsuran, nasabah, laporan koperasi">
+    <meta name="author" content="KSP Pekali 99">
+
+    <meta property="og:title" content="Sistem Informasi KSP Pekali 99">
+    <meta property="og:description"
+        content="Sistem Informasi Koperasi Simpan Pinjam Pekali 99 untuk mengelola data nasabah, pinjaman, angsuran, dan laporan koperasi secara digital.">
+    <meta property="og:url" content="https://ksppekali99.web.id">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="KSP Pekali 99">
+    <meta property="og:image" content="{{ asset('assets/img/logo-koperasi.png') }}">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Sistem Informasi KSP Pekali 99">
+    <meta name="twitter:description"
+        content="Sistem Informasi Koperasi Simpan Pinjam Pekali 99 untuk mengelola data nasabah, pinjaman, angsuran, dan laporan koperasi secara digital.">
+    <meta name="twitter:image" content="{{ asset('assets/img/logo-koperasi.png') }}">
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/logo-koperasi.png') }}">
 
     <!-- Google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
     <!-- Icon fonts -->
-    <link rel="stylesheet" href="assets/fonts/fontawesome.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.css">
-    <link rel="stylesheet" href="assets/fonts/linearicons.css">
-    <link rel="stylesheet" href="assets/fonts/open-iconic.css">
-    <link rel="stylesheet" href="assets/fonts/pe-icon-7-stroke.css">
-    <link rel="stylesheet" href="assets/fonts/feather.css">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/ionicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/linearicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/open-iconic.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/pe-icon-7-stroke.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/feather.css') }}">
 
     <!-- Core stylesheets -->
-    <link rel="stylesheet" href="assets/css/bootstrap-material.css">
-    <link rel="stylesheet" href="assets/css/shreerang-material.css">
-    <link rel="stylesheet" href="assets/css/uikit.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-material.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/shreerang-material.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/uikit.css') }}">
 
     <!-- Libs -->
-    <link rel="stylesheet" href="assets/libs/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="{{ asset('assets/libs/perfect-scrollbar/perfect-scrollbar.css') }}">
+
     <!-- Page -->
-    <link rel="stylesheet" href="assets/css/pages/authentication.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/authentication.css') }}">
 
     <style>
         .login-page {
@@ -51,12 +68,9 @@
             left: 0;
             width: 100%;
             height: 100%;
-
             background: url('{{ asset('assets/img/logo-koperasi.png') }}') no-repeat center;
             background-size: 700px;
-
             opacity: 0.15;
-            /* Atur transparansi gambar */
             z-index: -1;
         }
     </style>
@@ -77,10 +91,11 @@
             <div class="d-flex justify-content-center align-items-center">
                 <div class="ui-w-60">
                     <div class="w-100 position-relative">
-                        <img src="assets/img/logo-koperasi.png" alt="Brand Logo" class="img-fluid">
+                        <img src="{{ asset('assets/img/logo-koperasi.png') }}" alt="Logo KSP Pekali 99" class="img-fluid">
                     </div>
                 </div>
             </div>
+
             <hr style="border-top: 1px solid rgba(255,255,255,0.3);">
             <!-- [ Logo ] End -->
 
@@ -89,26 +104,26 @@
             <!-- [ Form ] Start -->
             <form class="my-4" action="{{ route('login.proses') }}" method="POST">
                 @csrf
+
                 <div class="form-group">
                     <label class="form-label">Username</label>
-                    <input type="text" name="username" class="form-control bg-secondary text-white border-0"
-                        required>
+                    <input type="text" name="username" class="form-control bg-secondary text-white border-0" required>
                     <div class="clearfix"></div>
                 </div>
+
                 <div class="form-group">
                     <label class="form-label d-flex justify-content-between align-items-end">
                         <span>Password</span>
-                        <a href="pages_authentication_password-reset.html" class="d-block small">Forgot password?</a>
                     </label>
-                    <input type="password" name="password" class="form-control bg-secondary text-white border-0"
-                        required>
+                    <input type="password" name="password" class="form-control bg-secondary text-white border-0" required>
                     <div class="clearfix"></div>
                 </div>
+
                 <div class="d-flex justify-content-between align-items-center m-0">
                     <label class="custom-control custom-checkbox m-0">
                         <input type="checkbox" class="custom-control-input">
-                        {{-- <span class="custom-control-label">Remember me</span> --}}
                     </label>
+
                     <button type="submit" class="btn btn-primary btn-sm">
                         Login
                     </button>
@@ -117,8 +132,7 @@
             <!-- [ Form ] End -->
 
             <div class="text-center text-muted">
-                Lupa password silahkan
-                <a href="pages_authentication_register-v1.html">Hubungi.</a>
+                Lupa password? Silakan hubungi admin KSP Pekali 99.
             </div>
 
         </div>
@@ -126,20 +140,19 @@
     <!-- [ content ] End -->
 
     <!-- Core scripts -->
-    <script src="assets/js/pace.js"></script>
-    <script src="assets/js/jquery-3.3.1.min.js"></script>
-    <script src="assets/libs/popper/popper.js"></script>
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/sidenav.js"></script>
-    {{-- <script src="assets/js/layout-helpers.js"></script> --}}
-    <script src="assets/js/material-ripple.js"></script>
+    <script src="{{ asset('assets/js/pace.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/js/sidenav.js') }}"></script>
+    <script src="{{ asset('assets/js/material-ripple.js') }}"></script>
 
     <!-- Libs -->
-    <script src="assets/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('assets/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
 
     <!-- Demo -->
-    <script src="assets/js/demo.js"></script>
-    <script src="assets/js/analytics.js"></script>
+    <script src="{{ asset('assets/js/demo.js') }}"></script>
+    <script src="{{ asset('assets/js/analytics.js') }}"></script>
 </body>
 
 </html>
